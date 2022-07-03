04dd050d857e383d2ea2c15054e6605901bc42d5
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-07-03 00:43:32 -0300

    chore: rooms header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import type { NextPage } from 'next'
+/* eslint-disable react/no-children-prop */
+import { Box, Button, ButtonGroup, Center, Flex, GridItem, Input, InputGroup, InputLeftElement, SimpleGrid } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { MagnifyingGlass } from 'phosphor-react';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Center h="100vh">
+      <Box bg="gray.700" p="4" rounded="4" w="3xl">
+        <Flex as="header" justify="space-between">
+          <InputGroup w="initial">
+            <InputLeftElement children={<MagnifyingGlass size={20}/>} />
+            <Input type="text" placeholder="Buscar jogos" />
+          </InputGroup>
+
+          <ButtonGroup size="md" spacing="4" colorScheme="blue">
+            <Button>Log in</Button>
+            <Button variant="outline">Sign Up</Button>
+          </ButtonGroup>
+        </Flex>
+        <SimpleGrid>
+          <GridItem>
+          </GridItem>
+        </SimpleGrid>
+      </Box>
+    </Center>
   )
 }
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,10 @@ import { extendTheme } from "@chakra-ui/react";
 
 
 export const theme = extendTheme({
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'  
+  },
   styles: {
     global: {
       body: {
